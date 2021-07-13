01aed907518359c1f4c8afd6fd9dbb4e5b79e0c4
Steph Rabbani <jordiwes@gmail.com>
2021-07-13 15:27:40 -0700

    folio adjustments based on ux assuarance (#121)

--- ppr-ui/src/components/common/FolioNumberSummary.vue
+++ ppr-ui/src/components/common/FolioNumberSummary.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container flat class="pa-0" id="folio-summary">
     <v-row no-gutters>
-      <v-col class="generic-label"><h3>Folio or Reference Number</h3></v-col>
+      <v-col class="generic-label"><h3>1. Folio or Reference Number</h3></v-col>
     </v-row>
     <v-row no-gutters class="pb-6 pt-4">
       <v-col>
@@ -24,6 +24,7 @@
                 label="Folio or Reference Number (Optional)"
                 v-model="folioNumber"
                 persistent-hint
+                :rules="rules"
               />
             </v-col>
           </v-row>
@@ -34,7 +35,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs, watch, onMounted } from '@vue/composition-api'
+import {
+  defineComponent,
+  reactive,
+  toRefs,
+  watch,
+  onMounted
+} from '@vue/composition-api'
 import { useGetters, useActions } from 'vuex-composition-helpers'
 
 export default defineComponent({
@@ -47,7 +54,11 @@ export default defineComponent({
     ])
 
     const localState = reactive({
-      folioNumber: ''
+      folioNumber: '',
+      rules: [
+        (v: string) => /^[0-9A-Za-z]*$/.test(v) || 'Invalid character', // numbers and letters only
+        (v: string) => !v || v.length <= 30 || 'Maximum 30 characters reached' // maximum character count
+      ]
     })
 
     watch(
