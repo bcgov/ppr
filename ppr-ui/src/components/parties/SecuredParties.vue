455ccafcefe29ff20a3ead4784dc30ff8e9569d5
Stephanie Rabbani <jordiwes@gmail.com>
2021-08-31 10:50:40 -0700

    test for the modal showing

--- ppr-ui/src/components/parties/SecuredParties.vue
+++ ppr-ui/src/components/parties/SecuredParties.vue
@@ -381,6 +381,7 @@ export default defineComponent({
     const fetchOtherSecuredParties = async () => {
       localState.loading = true
       if (parties.registeringParty) {
+        parties.registeringParty.businessName = 'Bank of Nova Scotia'
         // go to the service and see if there are similar secured parties
         const response: [SearchPartyIF] = await partyCodeSearch(
           parties.registeringParty.businessName,
