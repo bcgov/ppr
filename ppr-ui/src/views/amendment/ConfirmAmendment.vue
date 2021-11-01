fdcfaf442a39eee42727ffc3c813adaff11208a6
Doug Lovett <doug@diamante.ca>
2021-11-01 11:58:02 -0700

    Updates to setup of save registration API call.
    Signed-off-by: Doug Lovett <doug@diamante.ca>

--- ppr-ui/src/views/amendment/ConfirmAmendment.vue
+++ ppr-ui/src/views/amendment/ConfirmAmendment.vue
@@ -382,6 +382,11 @@ export default class ConfirmAmendment extends Vue {
   private get courtOrderValid (): boolean {
     const courtOrder: CourtOrderIF = this.getCourtOrderInformation
     return (!courtOrder ||
+            (courtOrder.courtName.length === 0 &&
+            courtOrder.courtRegistry.length === 0 &&
+            courtOrder.fileNumber.length === 0 &&
+            courtOrder.orderDate.length === 0 &&
+            courtOrder.effectOfOrder.length === 0) ||
             (courtOrder.courtName.length > 0 &&
             courtOrder.courtRegistry.length > 0 &&
             courtOrder.fileNumber.length > 0 &&
