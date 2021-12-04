698d14bd3c783680c4a43f15134ad390780729d8
Doug Lovett <doug@diamante.ca>
2021-12-04 14:17:26 -0800

    10126,10127 add helpdesk and update unit tests.
    Signed-off-by: Doug Lovett <doug@diamante.ca>

--- ppr-api/test_data/postgres_data_files/test_user_update.sql
+++ ppr-api/test_data/postgres_data_files/test_user_update.sql
@@ -9,3 +9,14 @@ UPDATE drafts
  WHERE id >= 200000000
    AND user_id IS NULL
 ;
+
+-- staff roles other account registrations
+INSERT INTO user_extra_registrations(id, account_id, registration_number, removed_ind)
+  VALUES(200000004, 'ppr_staff', 'TEST0001', null)
+;
+INSERT INTO user_extra_registrations(id, account_id, registration_number, removed_ind)
+  VALUES(200000005, 'helpdesk', 'TEST0001', null)
+;
+INSERT INTO user_extra_registrations(id, account_id, registration_number, removed_ind)
+  VALUES(200000006, 'gov_account_user', 'TEST0001', null)
+;
