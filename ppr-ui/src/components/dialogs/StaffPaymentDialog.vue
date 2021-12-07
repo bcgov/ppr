5ce7ed8e6275a25d752b9b04df858dcbc978b8c5
Stephanie Rabbani <jordiwes@gmail.com>
2021-12-07 15:15:34 -0800

    reset data on cancel

--- ppr-ui/src/components/dialogs/StaffPaymentDialog.vue
+++ ppr-ui/src/components/dialogs/StaffPaymentDialog.vue
@@ -1,25 +1,29 @@
 <template>
-  <base-dialog :setDisplay="display" :setOptions="options" @proceed="proceed($event)">
+  <base-dialog
+    :setDisplay="display"
+    :setOptions="options"
+    @proceed="proceed($event)"
+  >
     <template v-slot:content>
-              <staff-payment-component
-                :staffPaymentData="staffPaymentData"
-                :validate="validate"
-                :displaySideLabel="false"
-                :displayPriorityCheckbox="false"
-                @update:staffPaymentData="onStaffPaymentDataUpdate($event)"
-                @valid="valid = $event"
-              />
-            <v-row no-gutters class="pt-4" v-if="showCertifiedCheckbox">
-              <v-col class="pl-2">
-                <v-checkbox
-                  class="mt-2"
-                  v-model="certify"
-                  id="certify-checkbox"
-                  label="Make this a Certified Search ($25.00)"
-                />
-              </v-col>
-            </v-row>
-          </template>
+      <staff-payment-component
+        :staffPaymentData="staffPaymentData"
+        :validate="validate"
+        :displaySideLabel="false"
+        :displayPriorityCheckbox="false"
+        @update:staffPaymentData="onStaffPaymentDataUpdate($event)"
+        @valid="valid = $event"
+      />
+      <v-row no-gutters class="pt-4" v-if="showCertifiedCheckbox">
+        <v-col class="pl-2">
+          <v-checkbox
+            class="mt-2"
+            v-model="certify"
+            id="certify-checkbox"
+            label="Make this a Certified Search ($25.00)"
+          />
+        </v-col>
+      </v-row>
+    </template>
   </base-dialog>
 </template>
 
@@ -69,7 +73,10 @@ export default defineComponent({
   },
   emits: ['proceed'],
   setup (props, { emit }) {
-    const { setStaffPayment, setSearchCertified } = useActions<any>(['setStaffPayment', 'setSearchCertified'])
+    const { setStaffPayment, setSearchCertified } = useActions<any>([
+      'setStaffPayment',
+      'setSearchCertified'
+    ])
     const { getStaffPayment } = useGetters<any>(['getStaffPayment'])
     const localState = reactive({
       validate: false,
@@ -120,7 +127,22 @@ export default defineComponent({
         } else {
           localState.validate = true
         }
+        // is false... they cancelled or closed the box
       } else {
+        // blank payment data when closing the box
+        const pd = {
+          option: StaffPaymentOptions.NONE,
+          routingSlipNumber: '',
+          bcolAccountNumber: '',
+          datNumber: '',
+          folioNumber: '',
+          isPriority: false
+        }
+        setStaffPayment(pd)
+        // reset certified
+        localState.certify = false
+        setSearchCertified(localState.certify)
+        localState.validate = false
         emit('proceed', val)
       }
     }
@@ -208,12 +230,13 @@ export default defineComponent({
   display: none;
 }
 
-::v-deep .theme--light.v-label, ::v-deep .theme--light.v-input input {
+::v-deep .theme--light.v-label,
+::v-deep .theme--light.v-input input {
   color: $gray7;
 }
 
 ::v-deep .theme--light.v-label--is-disabled {
-    color: rgba(0, 0, 0, 0.38);
+  color: rgba(0, 0, 0, 0.38);
 }
 
 ::v-deep .v-application--is-ltr .v-text-field .v-label {
