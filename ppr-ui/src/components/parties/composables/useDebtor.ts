76981eccd1b66206b2301492914b4b41fdee1cb4
Stephanie Rabbani <jordiwes@gmail.com>
2022-02-07 09:53:52 -0800

    has amendment really changed

--- ppr-ui/src/components/parties/composables/useDebtor.ts
+++ ppr-ui/src/components/parties/composables/useDebtor.ts
@@ -5,7 +5,7 @@ import { Months } from '@/resources/months'
 import { PartyAddressSchema } from '@/schemas'
 import { useParty } from '@/composables/useParty'
 import { ActionTypes, RegistrationFlowType } from '@/enums'
-import { checkAddress } from '@/composables/address/factories/address-factory'
+import { checkAddress, formatAddress } from '@/composables/address/factories/address-factory'
 import { cloneDeep, isEqual } from 'lodash'
 
 const initPerson = { first: '', middle: '', last: '' }
@@ -115,7 +115,14 @@ export const useDebtor = (props, context) => {
         }
       }
     }
-
+    // format the original address before the compare
+    if (localState.originalDebtor) {
+      localState.originalDebtor.address = formatAddress(localState.originalDebtor.address)
+    }
+    // format the original birthdate the exact same before the compare
+    if (localState.originalDebtor.birthDate && localState.originalDebtor.birthDate.length > 10) {
+      localState.originalDebtor.birthDate = localState.originalDebtor.birthDate.substring(0, 10) + 'T00:00:00-08:00'
+    }
     // if they didn't change anything, just exit
     if ((localState.registrationFlowType === RegistrationFlowType.AMENDMENT) &&
     isEqual(localState.currentDebtor, localState.originalDebtor)) {
