c318c3db25d69bcc367622112c222ed9eb1df51b
Stephanie Rabbani <jordiwes@gmail.com>
2022-02-08 11:28:54 -0800

    folio ux update

--- ppr-ui/src/components/search/SearchBar.vue
+++ ppr-ui/src/components/search/SearchBar.vue
@@ -41,8 +41,12 @@
           </v-tooltip>
         </span>
       </v-col>
-      <v-col v-if="!isStaffBcolReg && !isStaffSbc" align-self="end" cols="4">
-        <folio-number :defaultFolioNumber="folioNumber" @folio-number="updateFolioNumber"/>
+      <v-col v-if="!isStaffBcolReg && !isStaffSbc" align-self="end" cols="3">
+        <folio-number
+          :defaultFolioNumber="folioNumber"
+          @folio-number="updateFolioNumber"
+          @folio-error="folioError = $event"
+        />
       </v-col>
       <v-col align-self="end" cols="1" class="pl-3"/>
     </v-row>
@@ -250,6 +254,7 @@ export default defineComponent({
       autoCompleteSearchValue: '',
       confirmationDialog: false,
       folioNumber: props.defaultFolioNumber,
+      folioError: false,
       hideDetails: false,
       searchTypes: SearchTypes,
       searchValue: props.defaultSearchValue,
@@ -381,6 +386,9 @@ export default defineComponent({
       }
     }
     const searchCheck = async () => {
+      if (localState.folioError) {
+        return
+      }
       localState.validations = validateSearchAction(localState)
       if (localState.validations) {
         localState.autoCompleteIsActive = false
