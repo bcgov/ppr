757dda15bb94089e245ecaca5c3fd9a1f7fb307b
Kial Jinnah <kialj876@gmail.com>
2022-02-17 15:22:25 -0800

    added debuging for scroll issue
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- ppr-ui/src/components/tables/common/TableObserver.vue
+++ ppr-ui/src/components/tables/common/TableObserver.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="observer" />
+  <tr class="observer" style="height: 40px;"/>
 </template>
 
 <script>
@@ -12,14 +12,18 @@ export default {
     const options = this.options || {}
     this.observer = new IntersectionObserver(([entry]) => {
       if (entry && entry.isIntersecting) {
+        console.log('emitting intersect event')
         this.$emit('intersect', entry)
       }
     }, options)
-
+    console.log('starting observer..')
     this.observer?.observe(this.$el) // eslint-disable-line no-unused-expressions
+    console.log('done')
   },
   destroyed () {
+    console.log('disconnecting observer..')
     this.observer?.disconnect() // eslint-disable-line no-unused-expressions
+    console.log('done')
   }
 }
 </script>
