0aeb106dcebcb020b5d03c45f7518b1509f27c7c
Kial <kialj876@gmail.com>
2022-02-18 08:42:31 -0800

    only stop paging if 0 results come back (#596)
    * only stop paging if 0 results come back
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>
    
    * update 2 other places that set paging vlaue
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- ppr-ui/src/views/Dashboard.vue
+++ ppr-ui/src/views/Dashboard.vue
@@ -682,8 +682,7 @@ export default class Dashboard extends Vue {
         cloneDeep(this.myRegDataChildDrafts), cloneDeep(nextRegs.registrations), true)
       this.myRegDataHistory = this.myRegDataHistory.concat(updatedRegs.registrations)
     }
-    console.log(nextRegs.registrations?.length)
-    if (nextRegs.registrations?.length < 100) { this.myRegNoMorePages = true }
+    if (nextRegs.registrations?.length < 1) { this.myRegNoMorePages = true }
     this.myRegDataLoading = false
   }
 
@@ -743,7 +742,7 @@ export default class Dashboard extends Vue {
     if (error) {
       this.emitError(error)
     } else {
-      if (sortedRegs.registrations?.length < 100) { this.myRegNoMorePages = true }
+      if (sortedRegs.registrations?.length < 1) { this.myRegNoMorePages = true }
       // parent drafts from sorted list
       const draftsCollapsed = this.myRegHistoryDraftCollapse(
         cloneDeep(sortedDrafts.drafts), cloneDeep(sortedRegs.registrations), sorting)
@@ -858,7 +857,7 @@ export default class Dashboard extends Vue {
       const error = myRegHistory?.error || myRegDrafts?.error
       this.emitError(error)
     } else {
-      if (myRegHistory.registrations?.length < 100) { this.myRegNoMorePages = true }
+      if (myRegHistory.registrations?.length < 1) { this.myRegNoMorePages = true }
       // add child drafts to their base registration in registration history
       const histroyDraftsCollapsed = this.myRegHistoryDraftCollapse(
         myRegDrafts.drafts, myRegHistory.registrations, false)
