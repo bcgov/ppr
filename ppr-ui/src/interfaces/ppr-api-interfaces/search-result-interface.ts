1dfc463909eeb4f4ffba5866f874cf5a279c9aad
Kial Jinnah <kialj876@gmail.com>
2022-03-22 16:21:01 -0700

    cleanup
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- ppr-ui/src/interfaces/ppr-api-interfaces/search-result-interface.ts
+++ ppr-ui/src/interfaces/ppr-api-interfaces/search-result-interface.ts
@@ -3,11 +3,11 @@ import { ErrorIF, VehicleCollateralIF, SearchNameIF } from '@/interfaces'
 // Interface for a single search result matching the search criteria.
 export interface SearchResultIF {
   error?: ErrorIF
-  id?: number
-  matchType?: string // EXACT or SIMILAR
+  id: number
+  matchType: string // EXACT or SIMILAR
   registrationNumber?: string // Conditional: only returned with search by registration number.
-  baseRegistrationNumber?: string
-  createDateTime?: string // UTC ISO formmatted date and time of financing statement.
+  baseRegistrationNumber: string
+  createDateTime: string // UTC ISO formmatted date and time of financing statement.
   registrationType?: string
   vehicleCollateral?: VehicleCollateralIF // Conditional: included if not debtor search.
   debtor?: SearchNameIF // Conditional: included if business debtor name or indivdual debtor name search.
