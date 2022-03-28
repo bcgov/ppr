de1c2cd6ed919c1435e3f97c0da452fc43506b8a
Kial Jinnah <kial@Kials-MBP.vn.shawcable.net>
2022-03-28 11:29:18 -0700

    set inProgress to false if selected is empty list
    Signed-off-by: Kial Jinnah <kial@Kials-MBP.vn.shawcable.net>

--- ppr-api/src/ppr_api/models/search_request.py
+++ ppr-api/src/ppr_api/models/search_request.py
@@ -415,7 +415,7 @@ class SearchRequest(db.Model):  # pylint: disable=too-many-instance-attributes
                     history_list.append(search)
                     if from_ui:
                         # if api_result is null then the selections have not been finished
-                        search['inProgress'] = not mapping['api_result']
+                        search['inProgress'] = not mapping['api_result'] and mapping['api_result'] != []
                         search['userId'] = str(mapping['user_id'])
 
         return history_list
