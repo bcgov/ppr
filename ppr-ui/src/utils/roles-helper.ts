bead048b7adfdb61dd93bd8e7e01aa10ca701903
Dima K <dima1086@hotmail.com>
2022-05-26 12:32:07 -0700

    Check more roles for Tombstone titles (#806)

--- ppr-ui/src/utils/roles-helper.ts
+++ ppr-ui/src/utils/roles-helper.ts
@@ -1,14 +1,12 @@
 export function getDescriptiveUserRole (authRoles: Array<string>): string {
   const accessRole = []
 
-  if (authRoles.includes('ppr_staff')) return 'STAFF_PPR'
-  if (authRoles.includes('mhr_staff')) return 'STAFF_MHR'
-
   if (authRoles.includes('staff') || authRoles.includes('sbc') ||
-    authRoles.includes('helpdesk')) accessRole.push('STAFF')
+    authRoles.includes('helpdesk') || authRoles.includes('ppr_staff') ||
+    authRoles.includes('mhr_staff')) accessRole.push('STAFF')
   else accessRole.push('CLIENT')
 
-  if (authRoles.includes('ppr')) accessRole.push('PPR')
-  if (authRoles.includes('mhr')) accessRole.push('MHR')
+  if (authRoles.includes('ppr') || authRoles.includes('ppr_staff')) accessRole.push('PPR')
+  if (authRoles.includes('mhr') || authRoles.includes('mhr_staff')) accessRole.push('MHR')
   return accessRole.join('_')
 }
