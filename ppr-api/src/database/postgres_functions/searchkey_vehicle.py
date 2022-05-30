04fe3aa12fdf74c032785f5e53436e0518f4ecc2
Doug Lovett <doug@diamante.ca>
2022-05-30 14:07:45 -0700

    MHR search report UXA updates. (#808)
    * MHR search report UXA updates.
    
    Signed-off-by: Doug Lovett <doug@diamante.ca>
    
    * PPR registration updates from UXA review.
    
    Signed-off-by: Doug Lovett <doug@diamante.ca>
    
    * Missed report footer update to add MHR prefix.
    
    Signed-off-by: Doug Lovett <doug@diamante.ca>

--- ppr-api/src/database/postgres_functions/searchkey_vehicle.py
+++ ppr-api/src/database/postgres_functions/searchkey_vehicle.py
@@ -6,39 +6,40 @@ searchkey_vehicle = PGFunction(
     schema="public",
     signature="searchkey_vehicle(serial_number IN VARCHAR)",
     definition="""
-    RETURNS VARCHAR
-    LANGUAGE plpgsql
-    AS
-    $$
-DECLARE
-        v_search_key VARCHAR(25);
-        BEGIN
-        v_search_key := LPAD(SUBSTR(serial_number, LENGTH(serial_number) - 5, 6),6,'0');
-        v_search_key := REGEXP_REPLACE(
-                         REGEXP_REPLACE(
-                          REGEXP_REPLACE(
-                           REGEXP_REPLACE(
+CREATE OR REPLACE FUNCTION public.searchkey_vehicle(serial_number character varying)
+ RETURNS character varying
+ LANGUAGE plpgsql
+    AS $$
+    DECLARE
+            v_search_key VARCHAR(25);
+            BEGIN
+            v_search_key := REGEXP_REPLACE(serial_number, '[^0-9A-Za-z]','','gi');
+            v_search_key := LPAD(SUBSTR(v_search_key, LENGTH(v_search_key) - 5, 6),6,'0');
+            v_search_key := REGEXP_REPLACE(
                             REGEXP_REPLACE(
-                             REGEXP_REPLACE(
-                              REGEXP_REPLACE(
-                               REGEXP_REPLACE(
+                            REGEXP_REPLACE(
+                            REGEXP_REPLACE(
                                 REGEXP_REPLACE(
-                                 REGEXP_REPLACE(
-                                  REGEXP_REPLACE(v_search_key,'I','1','gi'),
-                                                'L','1','gi'),
-                                                'Z','2','gi'),
-                                                'H','4','gi'),
-                                                'Y','4','gi'),
-                                                 'S','5','gi'),
-                                                 'C','6','gi'),
-                                                 'G','6','gi'),
-                                                 'B','8','gi'),
-                                                 'O','0','gi'),
-                                                 '[^\w]+|[A-Za-z]+','0','gi');
-               v_search_key := LPAD(v_search_key,6,'0');											 
-        RETURN v_search_key;
-    END
-    ; 
-    $$;
+                                REGEXP_REPLACE(
+                                REGEXP_REPLACE(
+                                REGEXP_REPLACE(
+                                    REGEXP_REPLACE(
+                                    REGEXP_REPLACE(
+                                    REGEXP_REPLACE(v_search_key,'I','1','gi'),
+                                                    'L','1','gi'),
+                                                    'Z','2','gi'),
+                                                    'H','4','gi'),
+                                                    'Y','4','gi'),
+                                                    'S','5','gi'),
+                                                    'C','6','gi'),
+                                                    'G','6','gi'),
+                                                    'B','8','gi'),
+                                                    'O','0','gi'),
+                                                    '[^\w]+|[A-Za-z]+','0','gi');
+                v_search_key := LPAD(v_search_key,6,'0');											 
+            RETURN v_search_key;
+        END
+        ;
+    $$
     """
 )
