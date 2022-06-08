69cb01118b0747d5a317c8785536daa3b9d7fc38
Gary Fan <gary.fan@gov.bc.ca>
2022-06-08 11:27:11 -0700

    validation UI fix

--- ppr-ui/src/views/ConfirmMHRSearch.vue
+++ ppr-ui/src/views/ConfirmMHRSearch.vue
@@ -58,7 +58,7 @@
                 :staffPaymentData="staffPaymentData"
                 :validate="validatePayment"
                 :displaySideLabel="true"
-                :displayPriorityCheckbox="false"
+                :displayPriorityCheckbox="true"
                 @update:staffPaymentData="onStaffPaymentDataUpdate($event)"
                 @valid="staffPaymentValid = $event"
               />
