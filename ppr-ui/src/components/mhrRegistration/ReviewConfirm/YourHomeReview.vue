d0462485a8bdbd9b9b7eba573fc46355db747d57
Gary Fan <gary.fan@gov.bc.ca>
2022-07-26 13:31:41 -0700

    add Circa to year when the approximate checkbox is checked

--- ppr-ui/src/components/mhrRegistration/ReviewConfirm/YourHomeReview.vue
+++ ppr-ui/src/components/mhrRegistration/ReviewConfirm/YourHomeReview.vue
@@ -31,7 +31,7 @@
             <h3>Year of Manufacture</h3>
           </v-col>
           <v-col cols="9">
-            <p>{{ getMhrRegistrationHomeDescription.baseInformation.year || '(Not Entered)' }}</p>
+            <p>{{ manufactureYear }}</p>
           </v-col>
         </v-row>
         <v-row no-gutters class="pt-3 px-6">
@@ -179,6 +179,16 @@ export default defineComponent({
       }),
       engineerDisplayDate: computed((): string => {
         return yyyyMmDdToPacificDate(getMhrRegistrationHomeDescription.value?.engineerReportDate, true)
+      }),
+      manufactureYear: computed((): string => {
+        const year = getMhrRegistrationHomeDescription.value.baseInformation.year
+        if (!year) {
+          return '(Not Entered)'
+        }
+        if (getMhrRegistrationHomeDescription.value.baseInformation.circa) {
+          return 'Circa ' + year
+        }
+        return year
       })
     })
 
