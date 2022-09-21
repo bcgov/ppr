9cbd3ec9cadac50f896948268962270ad46cd9e9
Cameron Bowler <cameron@freshworks.io>
2022-09-21 16:30:40 -0700

    pr cleanup

--- ppr-ui/src/composables/mhrInformation/useMhrInformation.ts
+++ ppr-ui/src/composables/mhrInformation/useMhrInformation.ts
@@ -1,34 +1,6 @@
-import { useGetters } from 'vuex-composition-helpers'
-import {
-  MhrRegistrationDescriptionIF,
-  MhrRegistrationHomeLocationIF,
-  MhrRegistrationHomeOwnerGroupIF,
-  MhrRegistrationIF,
-  NewMhrRegistrationApiIF,
-  MhrTransferIF
-} from '@/interfaces'
-import { StaffPaymentIF } from '@bcrs-shared-components/interfaces'
+import { MhrTransferIF } from '@/interfaces'
 
 export const useMhrInformation = () => {
-  // const {
-  //   getMhrRegistrationHomeDescription,
-  //   getMhrRegistrationSubmittingParty,
-  //   getMhrRegistrationDocumentId,
-  //   getMhrAttentionReferenceNum,
-  //   getMhrRegistrationLocation,
-  //   getMhrRegistrationHomeOwnerGroups,
-  //   getStaffPayment
-  // } = useGetters<any>([
-  //   'getMhrRegistrationHomeDescription',
-  //   'getMhrRegistrationSubmittingParty',
-  //   'getMhrRegistrationDocumentId',
-  //   'getMhrAttentionReferenceNum',
-  //   'getMhrRegistrationLocation',
-  //   'getMhrRegistrationHomeOwnerGroups',
-  //   'getCertifyInformation',
-  //   'getStaffPayment'
-  // ])
-
   const initMhrTransfer = (): MhrTransferIF => {
     return {
       mhrNumber: '',
@@ -37,22 +9,6 @@ export const useMhrInformation = () => {
       submittingParty: {}
     }
   }
-
-  // const buildApiData = () => {
-  //   const data: NewMhrRegistrationApiIF = {
-  //     documentId: getMhrRegistrationDocumentId.value,
-  //     submittingParty: parseSubmittingParty(),
-  //     ownerGroups: parseOwnerGroups(),
-  //     location: parseLocation(),
-  //     description: parseDescription()
-  //   }
-  //
-  //   if (getMhrAttentionReferenceNum.value) {
-  //     data.attentionReferenceNum = getMhrAttentionReferenceNum.value
-  //   }
-  //
-  //   return data
-  // }
 
   return {
     initMhrTransfer
