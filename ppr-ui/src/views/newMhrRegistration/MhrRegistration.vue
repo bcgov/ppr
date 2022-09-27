a573fbd1fb271e1d8a8f6c33034c3b69f74cf7d4
owillborn <Otto.Willborn@gov.bc.ca>
2022-09-27 08:26:05 -0700

    Reset all form validations upon MH registration submission

--- ppr-ui/src/views/newMhrRegistration/MhrRegistration.vue
+++ ppr-ui/src/views/newMhrRegistration/MhrRegistration.vue
@@ -184,6 +184,27 @@ export default defineComponent({
       localState.dataLoaded = true
     })
 
+    const resetAllValidations = (): void => {
+      // Reset your home validations
+      setValidation(MhrSectVal.YOUR_HOME_VALID, MhrCompVal.MAKE_MODEL_VALID, false)
+      setValidation(MhrSectVal.YOUR_HOME_VALID, MhrCompVal.HOME_SECTION_VALID, false)
+      setValidation(MhrSectVal.YOUR_HOME_VALID, MhrCompVal.HOME_CERTIFICATION_VALID, false)
+      setValidation(MhrSectVal.YOUR_HOME_VALID, MhrCompVal.REBUILT_STATUS_VALID, false)
+      setValidation(MhrSectVal.YOUR_HOME_VALID, MhrCompVal.OTHER_VALID, false)
+
+      // Reset submitting party validations
+      setValidation(MhrSectVal.SUBMITTING_PARTY_VALID, MhrCompVal.SUBMITTER_VALID, false)
+      setValidation(MhrSectVal.SUBMITTING_PARTY_VALID, MhrCompVal.DOC_ID_VALID, false)
+      setValidation(MhrSectVal.SUBMITTING_PARTY_VALID, MhrCompVal.REF_NUM_VALID, false)
+
+      // Reset home owner validations
+      setValidation(MhrSectVal.HOME_OWNERS_VALID, MhrCompVal.OWNERS_VALID, false)
+
+      // Reset home location validations
+      setValidation(MhrSectVal.LOCATION_VALID, MhrCompVal.LOCATION_TYPE_VALID, false)
+      setValidation(MhrSectVal.LOCATION_VALID, MhrCompVal.CIVIC_ADDRESS_VALID, false)
+    }
+
     const submit = async () => {
       // Prompt App Validations
       await setValidation(MhrSectVal.REVIEW_CONFIRM_VALID, MhrCompVal.VALIDATE_APP, true)
@@ -194,7 +215,7 @@ export default defineComponent({
         localState.submitting = true
         const mhrSubmission = await submitMhrRegistration(buildApiData(), parseStaffPayment())
         localState.submitting = false
-
+        resetAllValidations()
         mhrSubmission?.mhrNumber
           ? await context.root.$router.push({ name: RouteNames.DASHBOARD })
           : console.log(mhrSubmission?.error) // Handle Schema or Api errors here..
