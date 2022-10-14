a099dda9a4f9a4cf9f1738955926d52b28b4edd5
Cameron Bowler <cameron@freshworks.io>
2022-10-14 11:17:12 -0700

    fixed header dateTime label

--- ppr-ui/src/components/tombstone/TombstoneDischarge.vue
+++ ppr-ui/src/components/tombstone/TombstoneDischarge.vue
@@ -7,7 +7,7 @@
       <v-col class="ml-16 tombstone-info" style="padding-top: 0.375rem;">
         <v-row justify="end" no-gutters>
           <v-col :class="$style['info-label']" cols="6">
-            <span class="float-right">Base Registration Date and Time: </span>
+            <span class="float-right">{{ dateTimePrefix }} Registration Date and Time: </span>
           </v-col>
           <v-col class="pl-3" cols="6">
             {{ creationDate }}
@@ -84,6 +84,9 @@ export default defineComponent({
       registrationType: computed((): string => {
         const registration = getRegistrationType.value as RegistrationTypeIF
         return registration?.registrationTypeUI || ''
+      }),
+      dateTimePrefix: computed(() => {
+        return getRegistrationNumber.value ? 'Base' : 'MH'
       })
     })
 
