fe0147fc9968ba07061046612ef2f89db2ab59f3
owillborn <Otto.Willborn@gov.bc.ca>
2022-12-05 07:52:30 -0800

    Add Business text changes. Help section updates

--- ppr-ui/src/components/mhrRegistration/HomeOwners/AddEditHomeOwner.vue
+++ ppr-ui/src/components/mhrRegistration/HomeOwners/AddEditHomeOwner.vue
@@ -58,20 +58,89 @@
                   business or the incorporation number (including Societies and
                   extra-provincial companies registered in B.C.). If the name of
                   the organization does not appear in the look-up, enter the
-                  full legal name of the organization.
+                  full legal name of the
+                  <v-tooltip
+                    top
+                    content-class="top-tooltip pa-5"
+                    transition="fade-transition"
+                    data-test-id="suffix-tooltip"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <span
+                        v-bind="attrs"
+                        v-on="on"
+                      ><u>organization.</u></span>
+                    </template>
+                    Organizations, other than active B.C. businesses, that can be listed as owners
+                    include the following:<br><br>
+                    <li>Indian Bands,</li>
+                    <li>Public Bodies, or</li>
+                    <li>Organizations not registered in B.C.</li><br>
+                    Refer to "Help with Business and Organization Owners" for more details.
+                  </v-tooltip>
                 </p>
 
                 <simple-help-toggle
                   toggleButtonTitle="Help with Sole Proprietorships and Partnerships"
                 >
                   <h3 class="text-center mb-2">
-                    Help with Sole Proprietorships and Partnerships
+                    Business and Organization Owners
                   </h3>
                   <p>
-                    Registered owners of a manufactured home cannot be a sole
-                    proprietorship, partnership or limited partnership. The home
-                    must be registered in the name of the sole proprietor or
-                    partner (person or business).
+                    Businesses and organizations that <b>can</b> own a manufactured home include the following:
+                  </p>
+                  <h3 class="mb-2">
+                    B.C. Based Businesses
+                  </h3>
+                  <li>B.C. corporations</li>
+                  <li>B.C. societies</li>
+                  <li>B.C. cooperatives</li>
+                  <li>Extra-provincial companies registered in B.C. (corporations, societies and cooperatives)</li><br>
+                  <h3 class="mb-2">
+                    Other Businesses and Organizations
+                  </h3>
+                  <li>Indian Bands</li>
+                  <li>Public Bodies</li>
+                  <li>Businesses and Organizations not registered in B.C.</li><br>
+                  <p>Businesses and organizations that <b>cannot</b> own a manufactured home:</p>
+                  <h3 class="mb-2">
+                    Sole Proprietorships / Partnerships
+                  </h3>
+                  <p>
+                    Registered owners of a manufactured home <b>cannot</b> be a sole proprietorship, partnership,
+                    or limited partnership. The owners of the proprietorship or partnership must be added as a person
+                    or as an organization.
+                  </p>
+                  <hr class="mt-3 mb-5"/>
+                  <h3 class="text-center mb-2">
+                    When B.C. Based Businesses Must be in Active Status
+                  </h3>
+                  <p><b>New owners:</b> Must be active at the time of registration.</p>
+                  <p>
+                    If you are adding a B.C. based business as a new owner, the business
+                    <b>must be active on the B.C Corporate Register at the time of the registration.</b>
+                  </p><br>
+                  <p><b>Existing owners:</b> Must be active at the time the bill of sale was signed.</p>
+                  <p>
+                    If you are including a business that is already an owner of the home, the business
+                    <b>must have been active on the B.C Corporate Register at the time the bill of sale was signed</b>
+                  </p>
+                  <hr class="mt-3 mb-5" />
+                  <h3 class="text-center mb-2">
+                    My Business Isn't Listed
+                  </h3>
+                  <p>
+                    The business look-up displays the list of all active businesses in B.C. If your business is listed,
+                    select the business from the look-up list.
+                  </p>
+                  <p>
+                    If you enter the name of a B.C. based business and the name does not appear in the business look-up,
+                    the business is not active in the B.C. Corporate Register. In this case, please contact the
+                    Manufactured Home Registry.
+                  </p>
+                  <p>
+                    If you enter the name of another type of organization, the name will not appear in the look-up.
+                    In this case, type the full legal name of the organization.
                   </p>
                 </simple-help-toggle>
               </v-col>
@@ -82,7 +151,7 @@
                   filled
                   id="org-name"
                   ref="orgNameSearchField"
-                  label="Full Legal Name of Business or Organization"
+                  label="Find or enter the Full Legal Name of the Business or Organization"
                   v-model="searchValue"
                   :rules="orgNameRules"
                   persistent-hint
@@ -531,8 +600,17 @@ export default defineComponent({
 <style lang="scss" scoped>
 @import '@/assets/styles/theme.scss';
 
+u {
+    border-bottom: 1px dotted #000;
+    text-decoration: none;
+}
+
 #addHomeOwnerForm {
   p {
+    color: $gray7;
+    line-height: 24px;
+  }
+  li {
     color: $gray7;
     line-height: 24px;
   }
