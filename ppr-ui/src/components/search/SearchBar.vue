7855361a1895c183779bb960e66f939e75af3acf
owillborn <Otto.Willborn@gov.bc.ca>
2023-01-24 14:07:13 -0800

    Refactoring

--- ppr-ui/src/components/search/SearchBar.vue
+++ ppr-ui/src/components/search/SearchBar.vue
@@ -87,7 +87,7 @@
                   filled
                   :hint="searchHintFirst"
                   persistent-hint
-                  label="First Name"
+                  :label="optionFirst"
                   v-model="searchValueFirst"
                   @keypress.enter="searchCheck()"
                 />
@@ -374,6 +374,10 @@ export default defineComponent({
       }),
       searchMessage: computed((): string => {
         return localState.validations?.searchValue?.message || ''
+      }),
+      optionFirst: computed((): string => {
+        return isRoleStaffReg.value && isMHRSearchType(localState.selectedSearchType?.searchTypeAPI)
+          ? 'First Name (Optional)' : 'First Name'
       }),
       typeOfSearch: computed((): string => {
         // only show the type of search if authorized to both types
