630361b024804556399d45dca171194d4e1e2606
owillborn <Otto.Willborn@gov.bc.ca>
2023-02-03 11:08:42 -0800

    Fix to handle drafts

--- ppr-ui/src/composables/mhrRegistration/useNewMhrRegistration.ts
+++ ppr-ui/src/composables/mhrRegistration/useNewMhrRegistration.ts
@@ -236,7 +236,7 @@ export const useNewMhrRegistration = () => {
     const draftFilings = await getMhrDrafts()
     const myMhrHistory = await mhrRegistrationHistory(true, sortOptions)
     const filteredMhrHistory = addHistoryDraftsToMhr(myMhrHistory, draftFilings, sortOptions)
-    setMhrTableHistory(myMhrHistory)
+    setMhrTableHistory(filteredMhrHistory)
   }
 
   function addHistoryDraftsToMhr (
@@ -246,11 +246,9 @@ export const useNewMhrRegistration = () => {
     MhRegistrationSummaryIF[] {
     const sortedDraftFilings = orderBy(mhrDrafts, ['createDateTime'], ['desc'])
 
-    const sortedMhrHistory = orderBy(mhrHistory, ['createDateTime'], ['desc'])
-
     var mhrTableData = []
     // add drafts to Registrations.
-    sortedMhrHistory.forEach(transfer => {
+    mhrHistory.forEach(transfer => {
       transfer.baseRegistrationNumber = transfer.mhrNumber
       //
       // Prepare existing changes
@@ -297,7 +295,7 @@ export const useNewMhrRegistration = () => {
         transfer.changes = orderBy(transfer.changes, ['createDateTime'], ['desc'])
       }
     })
-    if (sortOptions?.status !== mhApiStatusTypes.DRAFT) mhrTableData = sortedMhrHistory
+    if (sortOptions?.status !== mhApiStatusTypes.DRAFT) mhrTableData = mhrHistory
     return mhrTableData
   }
   /**
