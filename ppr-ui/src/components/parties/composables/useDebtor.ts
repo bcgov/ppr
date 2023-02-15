004478e90c5fc678a099a5e79e94ca0bb58d1757
Dima K <dima1086@hotmail.com>
2023-02-14 16:03:46 -0800

    Fix typo

--- ppr-ui/src/components/parties/composables/useDebtor.ts
+++ ppr-ui/src/components/parties/composables/useDebtor.ts
@@ -129,7 +129,7 @@ export const useDebtor = (props, context) => {
       const localMonth = originalDateOfBirth.toLocaleDateString('en-CA', { month: '2-digit' })
       const localDay = originalDateOfBirth.toLocaleDateString('en-CA', { day: '2-digit' })
 
-      localState.currentDebtor.birthDate = [localYear, localMonth, localDay].join('-') + 'T00:00:00-08:00'
+      localState.originalDebtor.birthDate = [localYear, localMonth, localDay].join('-') + 'T00:00:00-08:00'
     }
     // if they didn't change anything, just exit
     if ((localState.registrationFlowType === RegistrationFlowType.AMENDMENT) &&
