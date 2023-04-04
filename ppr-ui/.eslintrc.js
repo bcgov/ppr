b5b9528152f50351345de91cfd79ccbb381f3973
Cameron Bowler <cameron@freshworks.io>
2023-04-04 11:00:19 -0700

    Implements extensions

--- ppr-ui/.eslintrc.js
+++ ppr-ui/.eslintrc.js
@@ -6,11 +6,10 @@ module.exports = {
   extends: [
     'plugin:vue/essential',
     '@vue/standard',
-    '@vue/typescript'
-    // ToDo: enable and fix Lint issues when capacity.
-    // 'plugin:vue/base',
-    // 'plugin:vuetify/base',
-    // 'plugin:vuetify/recommended'
+    '@vue/typescript',
+    'plugin:vue/base',
+    'plugin:vuetify/base',
+    'plugin:vuetify/recommended'
   ],
   rules: {
     'vue/multi-word-component-names': 'off',
