0910549a18c433af75e76000be4f0c675744c0f1
Dima K <dima1086@hotmail.com>
2023-05-08 16:32:11 -0700

    Fix SJTransfer submission by setting deleted group type to Joint

--- ppr-ui/src/composables/mhrInformation/useMhrInformation.ts
+++ ppr-ui/src/composables/mhrInformation/useMhrInformation.ts
@@ -298,7 +298,8 @@ export const useMhrInformation = () => {
             return owner.individualName ? { ...owner, individualName: normalizeObject(owner.individualName) } : owner
           }),
           // Determine group tenancy type
-          type: ownerGroup.owners.filter(owner => owner.action === ActionTypes.REMOVED).length > 1
+          type: (ownerGroup.owners.filter(owner => owner.action === ActionTypes.REMOVED).length > 1 ||
+            getMhrTransferType.value?.transferType === ApiTransferTypes.SURVIVING_JOINT_TENANT)
             ? ApiHomeTenancyTypes.JOINT
             : getMhrTransferHomeOwnerGroups.value.length > 1
               ? ApiHomeTenancyTypes.NA
