839bf57812bf1a5e7536f96ba001bdc9f4b2207e
Orel Ben Neriah <77707952+orelbn@users.noreply.github.com>
2023-05-26 23:27:05 -0700

    Fix scroll to first element on filter (#1356)
    * Identifies first item in mhr table
    
    * Version Bump
    
    * Improves typing and removes unncessary assignment

--- ppr-ui/src/components/tables/RegistrationTable.vue
+++ ppr-ui/src/components/tables/RegistrationTable.vue
@@ -510,16 +510,19 @@ export default defineComponent({
     }
 
     const isFirstItem = (item: RegistrationSummaryIF | DraftResultIF): boolean => {
-      const draftItem = item as DraftResultIF
       const firstBaseReg = localState.registrationHistory[0].baseRegistrationNumber
       const firstDocId = localState.registrationHistory[0].documentId
-      if (item.baseRegistrationNumber && item.baseRegistrationNumber === firstBaseReg) {
-        return true
-      }
-      if (!item.baseRegistrationNumber && draftItem.documentId === firstDocId) {
-        return true
-      }
-      return false
+      return item.baseRegistrationNumber
+        ? item.baseRegistrationNumber === firstBaseReg
+        : (item as DraftResultIF).documentId === firstDocId
+    }
+
+    const isFirstItemMhr = (item: MhRegistrationSummaryIF | MhrDraftIF): boolean => {
+      const firstMhrNumber = localState.registrationHistory[0].mhrNumber
+      const firstDocId = localState.registrationHistory[0].draftNumber
+      return item.mhrNumber
+        ? item.mhrNumber === firstMhrNumber
+        : item.draftNumber === firstDocId
     }
 
     const isNewRegItem =
@@ -563,8 +566,10 @@ export default defineComponent({
       }
     }
 
-    const setRowRef = (item: RegistrationSummaryIF | DraftResultIF): string => {
-      const isFirst = isFirstItem(item)
+    const setRowRef = (item: RegistrationSummaryIF | DraftResultIF | MhRegistrationSummaryIF | MhrDraftIF): string => {
+      const isFirst = props.isPpr
+        ? isFirstItem(item as RegistrationSummaryIF | DraftResultIF)
+        : isFirstItemMhr(item as MhRegistrationSummaryIF | MhrDraftIF)
       const isNewReg = isNewRegItem(item)
       if (isFirst && isNewReg) return 'newAndFirstItem'
       if (isFirst) return 'firstItem'
