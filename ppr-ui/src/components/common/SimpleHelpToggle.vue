8deb82db2d02b387f4da5c0ab61620120ac08680
Orel Ben Neriah <Orel.BenNeriah@gov.bc.ca>
2023-06-19 10:00:31 -0700

    Utilize compute to improve readbility

--- ppr-ui/src/components/common/SimpleHelpToggle.vue
+++ ppr-ui/src/components/common/SimpleHelpToggle.vue
@@ -6,13 +6,12 @@
       class="help-btn px-0"
       :ripple="false"
       @click="isHelpContentOpen = !isHelpContentOpen"
+      data-test-id="help-toggle-btn"
     >
       <v-icon class="mr-1">
         mdi-help-circle-outline
       </v-icon>
-      {{ isHelpContentOpen ?
-          defaultHideText ? 'Hide Help' : 'Hide ' + title
-          : title }}
+      {{ title }}
     </v-btn>
     <v-expand-transition>
       <div v-show="isHelpContentOpen" class="help-content mb-10">
@@ -36,6 +35,7 @@
 </template>
 
 <script lang="ts">
+import { computed } from 'vue'
 import { defineComponent, reactive, toRefs } from 'vue-demi'
 
 export default defineComponent({
@@ -49,7 +49,8 @@ export default defineComponent({
   setup (props) {
     const localState = reactive({
       isHelpContentOpen: false,
-      title: props.toggleButtonTitle,
+      hideText: props.defaultHideText ? 'Hide Help' : 'Hide ' + props.toggleButtonTitle,
+      title: computed(() : string => localState.isHelpContentOpen ? localState.hideText : props.toggleButtonTitle),
       showBottomToggle: props.hasBottomHideToggle
     })
 
