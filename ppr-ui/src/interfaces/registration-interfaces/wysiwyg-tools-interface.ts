2de0b4e624dba49b6d374b06db6262a7ca4bd2c7
Cameron Bowler <cameron@freshworks.io>
2023-06-22 10:55:05 -0700

    linting fix

--- ppr-ui/src/interfaces/registration-interfaces/wysiwyg-tools-interface.ts
+++ ppr-ui/src/interfaces/registration-interfaces/wysiwyg-tools-interface.ts
@@ -1,8 +1,9 @@
+// Interface for TipTap toolkit configuration
 export interface WysiwygToolsIF {
-	id: string
-	tooltipText: string
-	action: string
-	hLevel: number,
-	isActiveClass: string
-	icon: string
+  id: string
+  tooltipText: string
+  action: string
+  hLevel: number,
+  isActiveClass: string
+  icon: string
 }
