89aa740caa947be1c7292f92ce5212004488ac51
Orel Ben Neriah <Orel.BenNeriah@gov.bc.ca>
2023-06-27 14:22:16 -0700

    Update test to use util

--- ppr-ui/tests/unit/CautionBox.spec.ts
+++ ppr-ui/tests/unit/CautionBox.spec.ts
@@ -1,42 +1,8 @@
-// Libraries
-import Vue, { nextTick } from 'vue'
-import Vuetify from 'vuetify'
-import { mount, createLocalVue, Wrapper } from '@vue/test-utils'
-
-// Components
 import { CautionBox } from '@/components/common'
-import { createPinia, setActivePinia } from 'pinia'
-import { useStore } from '../../src/store/store'
-
-Vue.use(Vuetify)
-
-const vuetify = new Vuetify({})
-setActivePinia(createPinia())
-const store = useStore()
-
-/**
- * Creates and mounts a component, so that it can be tested.
- *
- * @returns a Wrapper<SearchedResultPpr> object with the given parameters.
- */
-function createComponent (setMsg: string, setImportantWord?: string): Wrapper<any> {
-  const localVue = createLocalVue()
-  localVue.use(Vuetify)
-  document.body.setAttribute('data-app', 'true')
-
-  return mount((CautionBox as any), {
-    localVue,
-    propsData: { setMsg: setMsg, setImportantWord: setImportantWord },
-    store,
-    vuetify
-  })
-}
+import { createComponent } from './utils'
 
 describe('Caution box component tests', () => {
   let wrapper: any
-
-  beforeEach(async () => {
-  })
 
   afterEach(() => {
     wrapper.destroy()
@@ -45,7 +11,7 @@ describe('Caution box component tests', () => {
   it('renders caution box component with given text', () => {
     const testMsg = 'this is very important'
     const importantText = 'Important'
-    wrapper = createComponent(testMsg)
+    wrapper = createComponent(CautionBox, { setMsg: testMsg, setImportantWord: importantText })
     const cautionBoxTxt = wrapper.findAll('.caution-box')
     expect(cautionBoxTxt.length).toBe(1)
     expect(cautionBoxTxt.at(0).text()).toContain(testMsg)
@@ -55,7 +21,7 @@ describe('Caution box component tests', () => {
   it('renders caution box component with changed bold text', () => {
     const testMsg = 'this is very important'
     const importantText = 'Caution'
-    wrapper = createComponent(testMsg, importantText)
+    wrapper = createComponent(CautionBox, { setMsg: testMsg, setImportantWord: importantText })
     const cautionBoxTxt = wrapper.findAll('.caution-box')
     expect(cautionBoxTxt.length).toBe(1)
     expect(cautionBoxTxt.at(0).text()).toContain(testMsg)
