497a351bf48e11647eed69cf5508bd1543675c3a
Orel Ben Neriah <77707952+orelbn@users.noreply.github.com>
2023-07-06 10:14:55 -0700

    Updates-Scroll-To-Invalid-For-Mhr-Registration (#1412)
    * Adds scrollToInvalid for review and confirm
    
    * Version bump
    
    * Adds conditional check

--- ppr-ui/src/composables/mhrRegistration/useMhrValidations.ts
+++ ppr-ui/src/composables/mhrRegistration/useMhrValidations.ts
@@ -44,28 +44,44 @@ export const useMhrValidations = (validationState: any) => {
   }
 
   /** Scroll to first SECTION tag that is invalid in specified flag block. */
-  const scrollToInvalid =
-    async (flagSection: MhrSectVal, viewId: string, optionalFlags: Array<boolean> = null): Promise<boolean> => {
-      // Create an array of the _ordered_ validation flags
-      const flagBlockArr = Object.keys(validationState[flagSection].value)
-        .map(key => validationState[flagSection].value[key])
+  const scrollToInvalid = (flagSection: MhrSectVal, viewId: string): boolean => {
+    // Create an array of the _ordered_ validation flags
+    const flagBlockArr = Object.keys(validationState[flagSection].value)
+      .map(key => validationState[flagSection].value[key])
 
-      // Prepend optional flags to array (ie for review pages)
-      if (optionalFlags) flagBlockArr.unshift(...optionalFlags)
+    // Find the _first_ corresponding Section that is invalid in the specified view
+    const view = document.getElementById(viewId)
+    const invalidComponent = view?.getElementsByTagName('section')[flagBlockArr.indexOf(false)]
 
-      // Find the _first_ corresponding Section that is invalid in the specified view
-      const view = document.getElementById(viewId)
-      const invalidComponent = view.getElementsByTagName('section')[flagBlockArr.indexOf(false)]
-
-      // If there is an invalid component, scroll to it
-      if (invalidComponent) {
-        setTimeout(() => {
-          invalidComponent.scrollIntoView({ block: 'start', inline: 'nearest', behavior: 'smooth' })
-        }, 500)
-        return false
-      }
-      return true
+    // If there is an invalid component, scroll to it
+    if (invalidComponent) {
+      setTimeout(() => {
+        invalidComponent.scrollIntoView({ block: 'start', inline: 'nearest', behavior: 'smooth' })
+      }, 500)
+      return false
     }
+    return true
+  }
+
+  /** Scroll to first HEADER of invalid step in the view.
+   *  If all steps are valid scroll to first Section that is invalid
+   *  in the specified section area.
+   */
+  const scrollToInvalidReviewConfirm = (stepsValidation: Array<boolean>): boolean => {
+    // Find the _first_ corresponding step that is invalid in the specified view
+    const view = document.getElementById('mhr-review-confirm')
+    const invalidStep = view?.getElementsByTagName('header')[stepsValidation.indexOf(false)]
+
+    // If there is an invalid step, scroll to its header
+    if (invalidStep) {
+      setTimeout(() => {
+        invalidStep.scrollIntoView({ block: 'start', inline: 'nearest', behavior: 'smooth' })
+      }, 500)
+      return false
+    }
+
+    return scrollToInvalid(MhrSectVal.REVIEW_CONFIRM_VALID, 'mhr-review-confirm-components')
+  }
 
   return {
     MhrCompVal,
@@ -76,6 +92,7 @@ export const useMhrValidations = (validationState: any) => {
     getSectionValidation,
     getStepValidation,
     resetAllValidations,
-    scrollToInvalid
+    scrollToInvalid,
+    scrollToInvalidReviewConfirm
   }
 }
