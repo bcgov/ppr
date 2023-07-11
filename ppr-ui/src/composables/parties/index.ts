3fb9c15f1dfa720e944a476789f48d169b8ecc3d
Orel Ben Neriah <Orel.BenNeriah@gov.bc.ca>
2023-07-11 13:54:32 -0700

    Moving code to more appropriate placement

---
+++ ppr-ui/src/composables/parties/index.ts
@@ -0,0 +1,5 @@
+export * from './securedPartyFormValidator'
+export * from './useDebtor'
+export * from './useDebtorValidation'
+export * from './useSecuredParty'
+export * from './useSecuredPartyValidation'
