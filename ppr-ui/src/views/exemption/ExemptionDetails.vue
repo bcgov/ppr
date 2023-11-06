39806e35d8e1cbea1abb192dff1b308c0c88a5d5
RuoxuanPengBC <144158684+RuoxuanPengBC@users.noreply.github.com>
2023-11-06 15:44:35 -0800

    residential exemption staff text updates (#1614)
    * residential exemption staff text updates

--- ppr-ui/src/views/exemption/ExemptionDetails.vue
+++ ppr-ui/src/views/exemption/ExemptionDetails.vue
@@ -11,7 +11,7 @@
           class="mt-9"
           setMsg="The homeowner and home location information in the residential exemption form, the manufactured home
           registry, and the land title must align. If the current MHR registration information is inaccurate, the
-          register must be updated prior to proceeding with this Application for Residential Exemption."
+          register must be updated prior to proceeding with this application for Residential Exemption."
         />
 
         <SimpleHelpToggle
@@ -22,19 +22,19 @@
           <template #content>
             <article class="px-8 py-2">
               <h3 class="text-center">Need Help Verifying Manufactured Home and Owner Details?</h3>
-              <p class="pt-3">The homeowner and location information in each of the following must align:</p>
-              <p class="mt-7 mb-0">(a) this Application for Residential Exemption;</p>
+              <p class="pt-3">The homeowner and location information in each of the following must align on:</p>
+              <p class="mt-7 mb-0">(a) this application for Residential Exemption;</p>
               <p class="mb-0">(b) the current Manufactured Home Registry (MHR) registration; and,</p>
-              <p>(c) the registered or pending owners and legal land description on the current land title as recorded
-                in the British Columbia land title and survey system (ltsa.ca).</p>
+              <p>(c) the current land title as recorded in the British Columbia land title and survey system
+                (<a href="https://ltsa.ca/">ltsa.ca</a>)(LTSA).</p>
               <p class="mt-7">The home location and homeowner(s) named in the Manufactured Home Registry match the home
                 location and current ownership of the home on the Residential Exemption form.</p>
               <p class="mt-7 mb-0">You must have one of the following that shows the name and home location and at least
                 one of the homeowners as also being an owner of the land where the home is located:</p>
-              <p class="mb-0">- A Land Title Search, dated within 30 days of today or;</p>
-              <p>- A pending freehold transfer.</p>
+              <p class="mb-0">- a Land Title Search, dated within 30 days of today or</p>
+              <p>- evidence of a pending freehold transfer.</p>
               <p class="mt-7">Any differences must be resolved before you proceed. If the current MHR registration
-                information is inaccurate, the register must be updated prior to proceeding with this Application for
+                information is inaccurate, the register must be updated prior to proceeding with this application for
                 Residential Exemption.</p>
             </article>
           </template>
