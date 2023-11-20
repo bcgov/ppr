416e2e783c5585e3602a48638361b780c3d70272
Dima K <dima1086@hotmail.com>
2023-11-20 15:14:19 -0800

    Update MHRSearch unit test

--- ppr-ui/tests/unit/MHRSearch.spec.ts
+++ ppr-ui/tests/unit/MHRSearch.spec.ts
@@ -1,24 +1,15 @@
 // Libraries
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import VueRouter from 'vue-router'
-import { createPinia, setActivePinia } from 'pinia'
 import { useStore } from '../../src/store/store'
-import { shallowMount, createLocalVue } from '@vue/test-utils'
 
 // Components
 import { SearchedResultMhr } from '@/components/tables'
 import { MHRSearch } from '@/views'
 
 // Other
-import mockRouter from './MockRouter'
 import { mockedMHRSearchResponse } from './test-data'
-import { UIMHRSearchTypes } from '@/enums'
+import { RouteNames, UIMHRSearchTypes } from '@/enums'
+import { createComponent } from './utils'
 
-Vue.use(Vuetify)
-
-const vuetify = new Vuetify({})
-setActivePinia(createPinia())
 const store = useStore()
 
 // Prevent the warning "[Vuetify] Unable to locate target [data-app]"
@@ -34,25 +25,8 @@ describe('Search component', () => {
   let wrapper: any
   const { assign } = window.location
 
-  beforeEach(async () => {
-    // mock the window.location.assign function
-    delete window.location
-    window.location = { assign: jest.fn() } as any
-
-    // create a Local Vue and install router on it
-    const localVue = createLocalVue()
-    localVue.use(VueRouter)
-    const router = mockRouter.mock()
-    await router.push({ name: 'mhr-search' })
-    wrapper = shallowMount((MHRSearch as any), { localVue, store, router, vuetify })
-  })
-
-  afterEach(() => {
-    window.location.assign = assign
-    wrapper.destroy()
-  })
-
-  it('renders Search View with base components', () => {
+  it('renders Search View with base components', async () => {
+    wrapper = await createComponent(MHRSearch, { appReady: true }, RouteNames.MHRSEARCH)
     expect(wrapper.findComponent(MHRSearch).exists()).toBe(true)
     // doesn't render unless there are results
     expect(wrapper.find(searchMeta).exists()).toBe(false)
@@ -61,16 +35,20 @@ describe('Search component', () => {
     expect(wrapper.find(folioHeader).exists()).toBe(false)
     expect(wrapper.findComponent(SearchedResultMhr).exists()).toBe(false)
   })
+
   it('renders the Results component and displays search data elements with filled result set.', async () => {
     await store.setManufacturedHomeSearchResults(mockedMHRSearchResponse[UIMHRSearchTypes.MHRMHR_NUMBER])
+    wrapper = await createComponent(MHRSearch, { appReady: true }, RouteNames.MHRSEARCH)
     expect(wrapper.find(noResultsInfo).exists()).toBe(false)
     expect(wrapper.findComponent(SearchedResultMhr).exists()).toBe(true)
   })
+
   it('renders the Results component and displays search data elements with empty result set.', async () => {
     const response = mockedMHRSearchResponse[UIMHRSearchTypes.MHRMHR_NUMBER]
     response.totalResultsSize = 0
     response.results = []
     await store.setManufacturedHomeSearchResults(response)
+    wrapper = await createComponent(MHRSearch, { appReady: true }, RouteNames.MHRSEARCH)
     expect(wrapper.find(noResultsInfo).exists()).toBe(true)
     expect(wrapper.findComponent(SearchedResultMhr).exists()).toBe(true)
   })
