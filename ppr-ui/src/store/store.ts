cf36cc53191f3c335df66e0bb5c0a0f52b2947c9
Dima K <dima1086@hotmail.com>
2024-03-01 09:07:17 -0800

    Transport Permit Amended badges on Review page

--- ppr-ui/src/store/store.ts
+++ ppr-ui/src/store/store.ts
@@ -1303,6 +1303,11 @@ export const useStore = defineStore('assetsStore', () => {
     setUnsavedChanges(true)
   }
 
+  /** Store original Reg Status to compare it to updated status and show the Amended badge */
+  function setMhrTransportPermitRegStatus (status: string) {
+    state.value.mhrOriginalTransportPermit.registrationStatus = status
+  }
+
   /** Original Transport Permit filing when working with Amendments */
   function setMhrOriginalTransportPermit ({ key, value }) {
     state.value.mhrOriginalTransportPermit[key] = value
@@ -1723,6 +1728,7 @@ export const useStore = defineStore('assetsStore', () => {
     setMhrTransportPermitNewLocation,
     setMhrTransportPermitNewCivicAddress,
     setMhrOriginalTransportPermit,
+    setMhrTransportPermitRegStatus,
 
     // MHR Unit Notes
     setMhrUnitNoteType,
