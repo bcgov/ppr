c595b7780de9701eae7d8e433d33502141ad6a46
Dima K <dima1086@hotmail.com>
2024-03-12 00:26:48 -0400

    Update Owner help content for Transfers and Registrations (#1777)
    * Update Owner help content for Transfers and Registrations

--- ppr-ui/src/views/newMhrRegistration/HomeOwners.vue
+++ ppr-ui/src/views/newMhrRegistration/HomeOwners.vue
@@ -20,51 +20,66 @@
         <p class="mt-2 mb-0">
           Add a person or an organization as the owner of the home. You can add
           multiple owners to form joint tenancy or tenants in common ownership.
-          Note: Tenants in common ownership requires more than one group of
+          <strong>Note:</strong> Tenants in common ownership requires more than one group of
           owners.
         </p>
 
         <SimpleHelpToggle
           toggleButtonTitle="Help with Owners"
           :defaultHideText="false"
-          class="my-6"
+          class="my-6 help-with-owners"
         >
           <template #content>
-            <h3 class="text-center mb-2">
+            <h3 class="text-center mb-7">
               Help with Owners
             </h3>
-            <h4>Sole Owner</h4>
+            <h4>
+              Sole Ownership
+            </h4>
+            <p>This applies when the home is owned by a single individual or organization.</p>
+            <h4>Joint Tenancy</h4>
             <p>
-              This applies when the home is owned by a single individual or organization.
-            </p>
-            <h4>Joint Tenants</h4>
-            <p>
-              This applies when the home is jointly owned by a number of individuals or
-              organizations or some combination of the two.
+              This applies when the home is owned by two or more individuals and/or organizations jointly, with rights
+              of survivorship. In joint tenancy, if one joint tenant dies (or for an organization, if it ceases to
+              exist) their
+              ownership share in the home passes to the surviving joint owner(s).
             </p>
             <h4>Tenants in Common</h4>
             <p>
-              This applies when the home is owned by a number of groups or individuals or
-              organizations or some combination of the two (where a group could consist of
-              a single owner) and each group of owners has the right to dispose of their share
-              independent of the other owner groups and will be disposed of as part of the
-              estate in the case of a death.
+              This applies when the home is owned by two or more individuals and/or organizations that owns an undivided
+              share in the home. Ownership will be displayed in groups of owners with their undivided portion. Each
+              owner has the right to dispose of their share independently of the other owners, and in the case of the
+              death of an individual,
+              their share will belong to the estate of the individual, or through the legal process for dissolution of
+              assets, in the case of an organization.
+            </p>
+            <h4>Definition for Groups</h4>
+            <p>
+              A group is one or more owners of the home that have an equal undivided share of the home. Tenants in
+              common are
+              recorded in the Manufactured Home Registry in groups. The relationship between groups will always be as
+              tenants in
+              common. The relationship between owners within each group is a joint tenancy. There may be multiple groups
+              with
+              joint tenants.
+            </p>
+            <h4>Recording Ownership for Multiple Groups</h4>
+            <p>
+              Each group’s ownership share must be recorded as a portion of the total number of equal shares in the
+              home. For
+              example, if the home is owned by two groups, and each owns half of the home, this is recorded as each
+              group
+              holding 1 of 2 shares. If the home is owned by two groups but one group holds two-thirds and one group
+              holds
+              one-third, this is recorded as the first group holding 2 of 3 shares and the second group holding 1 of 3
+              shares.
             </p>
             <p>
-              Each group will hold a certain share of the home. To record this it is
-              necessary to express this as some number of a total number of equal shares.
-              For example, if the home is owned by two owner groups each of whom owns half of
-              the home this can be seen as each group holding 1 of 2 shares. If the home is
-              owned by two groups but one holds two-thirds and one holds the other third this
-              can be expressed as the first holding 2 of 3 shares and the second holding 1 of
-              3 shares.
-            </p>
-            <p>
-              When adding owners into groups, the total number of shares in a home (Total
-              Available) must be entered as well as the number of shares each group owns
-              (Amount Owned by the Group). It is recommended that all groups use the same
-              common denominator for Total Available, preferably using the lowest common
-              denominator.
+              When adding groups of owners, you must enter the total number of shares in the home (Total Available) and
+              the
+              number of shares owned by each group (Amount Owned by the Group). All groups should use the same
+              denominator as is
+              used for Total Available, preferably using the lowest common denominator for all groups.
             </p>
           </template>
         </SimpleHelpToggle>
@@ -688,6 +703,18 @@ export default defineComponent({
 @import '@/assets/styles/theme.scss';
 span:not(.generic-label)  {
   color: $gray7
+}
+
+.help-with-owners {
+  h4 {
+    font-size: 16px;
+    line-height: normal;
+    color: $gray7;
+  }
+
+  p {
+    margin-bottom: 24px;
+  }
 }
 
 .transfer-table-error {
