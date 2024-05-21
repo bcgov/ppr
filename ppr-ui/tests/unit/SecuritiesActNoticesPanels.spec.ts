b60e640f02f06a88aeace71f0b45a8cef359f7f4
Cameron Bowler <cameron.bowler@ca.ey.com>
2024-05-21 13:39:42 -0700

    Version bump and test fix

--- ppr-ui/tests/unit/SecuritiesActNoticesPanels.spec.ts
+++ ppr-ui/tests/unit/SecuritiesActNoticesPanels.spec.ts
@@ -1,12 +1,12 @@
-import { createComponent } from './utils'
-import { beforeEach, describe, expect, it } from 'vitest'
-import SecuritiesActNoticesPanels from '@/components/registration/securities-act-notices/SecuritiesActNoticesPanels.vue'
-import { AddEditSaNoticeIF } from '@/interfaces'
-import { SaNoticeTypes } from '@/enums'
 import { useStore } from '@/store/store'
 import { nextTick } from 'vue'
-import flushPromises from 'flush-promises'
 import NoticePanel from '@/components/registration/securities-act-notices/NoticePanel.vue'
+import SecuritiesActNoticesPanels from '@/components/registration/securities-act-notices/SecuritiesActNoticesPanels.vue'
+import { beforeEach } from 'vitest'
+import { createComponent } from './utils'
+import { SaNoticeTypes } from '@/enums'
+import flushPromises from 'flush-promises'
+import { AddEditSaNoticeIF } from '@/interfaces'
 
 const store = useStore()
 
@@ -16,16 +16,70 @@ const mockNotice: AddEditSaNoticeIF = {
   securitiesActOrders: []
 }
 
-describe('NoticePanel.vue', () => {
+describe('SecuritiesActNoticesPanel.vue', () => {
   let wrapper
-  beforeEach(async ()=> {
-    await store.setSecuritiesActNotices([mockNotice])
+  beforeEach(async () => {
     wrapper = await createComponent(SecuritiesActNoticesPanels, {
       isAddingNotice: false
     })
   })
 
-  it('renders a notice panel correctly', () => {
+  it('renders no notices message when no notices are available', async () => {
+    // Check if the no notices message is rendered
+    expect(wrapper.find('.empty-notices-msg').exists()).toBe(true)
+  })
+
+  it('renders notice panels when notices are available', async () => {
+    // Simulate having notices available
+    store.setSecuritiesActNotices([
+      {
+        noticeType: SaNoticeTypes.NOTICE_OF_LIEN,
+        effectiveDate: '2024-05-10'
+      },
+      {
+        noticeType: SaNoticeTypes.NOTICE_OF_PROCEEDINGS,
+        effectiveDate: '2024-05-10'
+      }
+    ])
+
+    await nextTick()
+
+    // Check if notice panels are rendered
+    expect(wrapper.findAllComponents(NoticePanel).length).toBe(2)
+  })
+
+  it('toggles panel when clicked', async () => {
+    // Simulate having notices available
+    store.setSecuritiesActNotices([
+      {
+        noticeType: SaNoticeTypes.NOTICE_OF_LIEN,
+        effectiveDate: '2024-05-10'
+      },
+      {
+        noticeType: SaNoticeTypes.NOTICE_OF_PROCEEDINGS,
+        effectiveDate: '2024-05-10'
+      }
+    ])
+
+    const panels = await wrapper.findAllComponents(NoticePanel)
+
+    // Click on the first panel
+    await panels.at(0).find('.security-notice-header-action').trigger('click')
+
+    // Check if the first panel is active
+    expect(wrapper.vm.activePanels).toEqual([0])
+
+    // Click on the second panel
+    await panels.at(1).find('.security-notice-header-action').trigger('click')
+
+    // Check if the second panel is active and the first active panel was closed
+    expect(wrapper.vm.activePanels).toEqual([1])
+  })
+
+  it('renders a notice panel content correctly', async () => {
+    await store.setSecuritiesActNotices([mockNotice])
+    await nextTick()
+
     expect(wrapper.find('.notice-type-text').text()).toBe('Notice of Lien')
     expect(wrapper.find('.effective-date-text').text()).toContain('Effective Date: January 1, 2024')
     expect(wrapper.find('.security-notice-header-action').exists()).toBe(true)
@@ -35,6 +89,9 @@ describe('NoticePanel.vue', () => {
   })
 
   it('removes a notice correctly', async () => {
+    await store.setSecuritiesActNotices([mockNotice])
+    await nextTick()
+
     expect(wrapper.find('.notice-type-text').text()).toBe('Notice of Lien')
     expect(wrapper.find('.effective-date-text').text()).toContain('Effective Date: January 1, 2024')
 
