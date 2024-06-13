ff87c76b5971377e3f574fb24c69ff60cb347a57
Dima K <dima1086@hotmail.com>
2024-06-13 05:55:56 -0700

    Create a new common CollapsibleCard component

---
+++ ppr-ui/src/components/common/CollapsibleCard.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-card
+    flat
+    class="collapsible-card"
+  >
+    <header class="review-header">
+      <label
+        class="font-weight-bold d-flex"
+        data-test-id="card-header-label"
+      >
+        <img
+          class="mr-1"
+          width="25"
+          src="@/assets/svgs/homeownersicon_reviewscreen.svg"
+        >
+        {{ headerLabel }}
+      </label>
+
+      <v-btn
+        variant="plain"
+        color="primary"
+        class="hide-help-btn px-0"
+        data-test-id="card-toggle-label"
+        :ripple="false"
+        @click="toggleCardOpen"
+      >
+        <v-icon
+          icon="mdi-eye"
+          class="mr-1"
+        />
+        {{ state.isCardOpen ? 'Show' : 'Hide' }} {{ toggleLabel }}
+      </v-btn>
+    </header>
+    <v-expand-transition>
+      <div
+        v-if="state.isCardOpen"
+        data-test-id="card-slots"
+      >
+        <!-- Information/Description text slot -->
+        <slot name="infoSlot" />
+
+        <slot name="mainSlot" />
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = defineProps<{
+  headerLabel: string,
+  toggleLabel: string
+}>()
+
+const state = reactive({
+  isCardOpen: true
+})
+
+const toggleCardOpen = () => {
+  state.isCardOpen = !state.isCardOpen;
+}
+
+</script>
+
+<style lang="scss" scoped>
+.collapsible-card {
+  .review-header {
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+</style>
---
+++ ppr-ui/tests/unit/common-components/CollapsibleCard.spec.ts
@@ -0,0 +1,44 @@
+import CollapsibleCard from '@/components/common/CollapsibleCard.vue'
+import { getTestId } from '../utils'
+import { mount } from '@vue/test-utils'
+import { nextTick } from 'vue'
+
+describe('CollapsibleCard', () => {
+  let wrapper: any
+
+  const HEADER_LABEL = 'Main Header Label'
+  const TOGGLE_LABEL = 'Toggle Label'
+
+  beforeEach(async () => {
+    wrapper = mount(CollapsibleCard, {
+      props: {
+        headerLabel: HEADER_LABEL,
+        toggleLabel: TOGGLE_LABEL
+      },
+      slots: {
+        infoSlot: '<div>Info Content</div>',
+        mainSlot: '<div>Main Content</div>'
+      }
+    })
+  })
+
+  it('renders component with props', async () => {
+    const collapsibleCard = wrapper.findComponent(CollapsibleCard)
+    expect(collapsibleCard.exists()).toBe(true)
+    expect(collapsibleCard.find(getTestId('card-header-label')).text()).toBe(HEADER_LABEL)
+    expect(collapsibleCard.find(getTestId('card-toggle-label')).text()).toBe('Show ' + TOGGLE_LABEL)
+
+    const cardSlots = collapsibleCard.find(getTestId('card-slots'))
+
+    expect(cardSlots.exists()).toBe(true)
+    expect(cardSlots.text()).toContain('Info Content')
+    expect(cardSlots.text()).toContain('Main Content')
+
+    // collapse the card and slots
+    collapsibleCard.vm.toggleCardOpen()
+    await nextTick()
+
+    expect(collapsibleCard.find(getTestId('card-slots')).exists()).toBe(false)
+    expect(collapsibleCard.find(getTestId('card-toggle-label')).text()).toBe('Hide ' + TOGGLE_LABEL)
+  })
+})
