3db8f06002b9f583a3e99e482d39b6809a788344
CameronEYDS <53542131+cameron-eyds@users.noreply.github.com>
2024-07-18 14:35:15 -0700

    Remove Court Order Fee Override (#1986)
    * remove override COU
    
    * remove unused import

--- ppr-ui/src/components/common/StickyContainer.vue
+++ ppr-ui/src/components/common/StickyContainer.vue
@@ -49,7 +49,7 @@ import {
 import { useStore } from '@/store/store'
 import { ButtonsStacked } from '@/components/common'
 import { FeeSummary } from '@/composables/fees'
-import { ApiTransferTypes, UIRegistrationTypes, UITransferTypes } from '@/enums'
+import { UIRegistrationTypes, UITransferTypes } from '@/enums'
 import { FeeSummaryTypes } from '@/composables/fees/enums'
 import {
   AdditionalSearchFeeIF,
@@ -149,8 +149,7 @@ export default defineComponent({
       getIsStaffClientPayment,
       isRoleStaffReg,
       isRoleStaffSbc,
-      getStaffPayment,
-      getMhrTransferType
+      getStaffPayment
     } = storeToRefs(useStore())
 
     const localState = reactive({
@@ -166,14 +165,6 @@ export default defineComponent({
       disableSubmitBtn: props.setDisableSubmitBtn,
       feeOverride: computed(() => {
         if (isNonBillable.value || localState.isNoFeePayment) {
-          return {
-            feeAmount: 0,
-            processingFee: null, // not used in override
-            quantity: null, // not used in override
-            serviceFee: getUserServiceFee.value as number
-          } as FeeSummaryI
-        }
-        if (getMhrTransferType.value?.transferType === ApiTransferTypes.COU) {
           return {
             feeAmount: 0,
             processingFee: null, // not used in override
