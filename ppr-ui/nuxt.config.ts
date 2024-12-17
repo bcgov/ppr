211385963a29f4fb38e4da606f52d9a76233980c
CameronEYDS <53542131+cameron-eyds@users.noreply.github.com>
2024-12-17 06:55:44 -0800

    Inlude FireBaseJson (#2087)

--- ppr-ui/nuxt.config.ts
+++ ppr-ui/nuxt.config.ts
@@ -23,7 +23,6 @@ const aboutText = (aboutText1 && aboutText2)
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  // components: true,
   components: [
     '~/components', // Auto-import components from the components directory
     '~/components/collateral',
