5990ceff71c0881af32caed5925593743aace48f
CameronEYDS <53542131+cameron-eyds@users.noreply.github.com>
2025-05-30 08:20:31 -0700

    Connect Layer QA (#2162)
    * Version bump and misc QA items
    
    * vaults update

--- ppr-ui/src/middleware/01.redirect.global.ts
+++ ppr-ui/src/middleware/01.redirect.global.ts
@@ -2,9 +2,16 @@ export default defineNuxtRouteMiddleware((to) => {
   if (import.meta.client) {
     const { $keycloak } = useNuxtApp()
     if (to.meta.requiresAuth && !$keycloak.authenticated) {
-      const registryHomeURL = useRuntimeConfig().public.VUE_APP_REGISTRY_URL
+      const registryHomeURL = removeSuffix(useRuntimeConfig().public.VUE_APP_REGISTRY_URL, '/dashboard')
       const redirectUrl = encodeURIComponent(window.location.href)
       window.location.href = `${registryHomeURL}/en-CA/login/?return=${redirectUrl}`
     }
   }
 })
+
+const removeSuffix = (url: string, suffix: string) => {
+  if (url.endsWith(suffix)) {
+    return url.slice(0, -suffix.length)
+  }
+  return url
+}
