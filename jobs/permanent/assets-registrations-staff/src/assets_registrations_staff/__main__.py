11d00f729b8bb0d1b7ac79da77b7ce4fcfc74083
Doug Lovett <doug@diamante.ca>
2025-06-10 10:40:28 -0700

    MHR API staff prune reg table job. (#2168)
    * MHR API staff prune reg table job.
    
    Signed-off-by: Doug Lovett <doug@diamante.ca>
    
    * Assets payment fix update statement typo.
    
    Signed-off-by: Doug Lovett <doug@diamante.ca>
    
    ---------
    
    Signed-off-by: Doug Lovett <doug@diamante.ca>

---
+++ jobs/permanent/assets-registrations-staff/src/assets_registrations_staff/__main__.py
@@ -0,0 +1,40 @@
+# Copyright © 2025 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This module is the entry point to initialize the job."""
+import json
+import os
+import sys
+
+from assets_registrations_staff.utils.logging import logger, setup_logging
+
+from .config import Config
+from .job import job
+
+# Retrieve Job-defined env vars
+TASK_INDEX = os.getenv("CLOUD_RUN_TASK_INDEX", "0")
+TASK_ATTEMPT = os.getenv("CLOUD_RUN_TASK_ATTEMPT", "0")
+
+
+# Start script
+if __name__ == "__main__":
+    try:
+        config = Config()
+        setup_logging(os.path.join(os.path.abspath(os.path.dirname(__file__)), "logging.yaml"))
+        logger.info(f"Assets staff registrations job starting. DB url={config.APP_DATABASE_URI}")
+        job(config)
+    except Exception as err:
+        message = f"Task #{TASK_INDEX}, " + f"Attempt #{TASK_ATTEMPT} failed: {str(err)}"
+        print(json.dumps({"message": message, "severity": "ERROR"}))
+        logger.error(f"Job failed: {str(err)}")
+        sys.exit(1)  # Retry Job Task by exiting the process
---
+++ jobs/permanent/assets-registrations-staff/src/assets_registrations_staff/utils/base.py
@@ -0,0 +1,56 @@
+# Copyright © 2025 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This module extends Enum for easier comparison."""
+
+from enum import Enum, EnumMeta, auto  # pylint: disable=W0611;# noqa: F401
+from typing import Optional
+
+
+class BaseMeta(EnumMeta):
+    """Meta class for the enum."""
+
+    def __contains__(self, other):  # pylint: disable=C0203
+        """Return True if 'in' the Enum."""
+        try:
+            self(other)  # pylint: disable=no-value-for-parameter
+        except ValueError:
+            return False
+
+        return True
+
+
+class BaseEnum(str, Enum, metaclass=BaseMeta):
+    """Replace autoname from Enum class."""
+
+    @classmethod
+    def get_enum_by_value(cls, value: str) -> Optional[str]:
+        """Return the enum by value."""
+        for enum_value in cls:
+            if enum_value.value == value:
+                return enum_value
+        return None
+
+    @classmethod
+    def get_enum_by_name(cls, value: str) -> Optional[str]:
+        """Return the enum by value."""
+        for enum_value in cls:
+            if enum_value.name == value:
+                return enum_value
+        return None
+
+    # pragma warning disable S5720; # noqa: E265
+    # disable sonar cloud complaining about this signature
+    def _generate_next_value_(name, start, count, last_values):  # pylint: disable=E0213;# noqa: N805
+        """Return the name of the key."""
+        return name
