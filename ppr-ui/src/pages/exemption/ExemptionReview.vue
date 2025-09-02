b43c6be7cafe3490e741fe07d764c8e887f7b3b7
Cameron Bowler <cameron.bowler@ca.ey.com>
2025-09-02 07:57:54 -0700

    Include Owner Lookup for Submit Party in Exemptions

--- ppr-ui/src/pages/exemption/ExemptionReview.vue
+++ ppr-ui/src/pages/exemption/ExemptionReview.vue
@@ -85,10 +85,11 @@
           code, you can look up the party code or name.
         </p>
 
-        <PartySearch
-          is-mhr-party-search
-          class="mb-8 rounded-all"
-          @select-item="handlePartySelect"
+        <PartySelectMethod
+          class="mt-4 mb-8 w-full"
+          is-transfer
+          @party="handlePartySelect($event)"
+          @owner="handlePartySelect($event)"
         />
 
         <FormCard
@@ -227,10 +228,12 @@ import { useExemptions, usePayment } from '@/composables'
 import { parseSubmittingPartyToAccountInfo, yyyyMmDdToPacificDate } from '@/utils'
 import { TransportPermitDetails } from '@/components/mhrTransportPermits'
 import CautionBox from '@/components/common/CautionBox.vue'
+import PartySelectMethod from '@/components/parties/PartySelectMethod.vue'
 
 export default defineComponent({
   name: 'ExemptionReview',
   components: {
+    PartySelectMethod,
     FolioOrReferenceNumber,
     AccountInfo,
     PartySearch,
@@ -317,7 +320,10 @@ export default defineComponent({
         businessName: party.businessName,
         address: party.address,
         emailAddress: party.emailAddress,
-        phoneNumber: party.contact.phoneNumber ? `${party.contact.areaCode}${party.contact.phoneNumber}` : '',
+        phoneNumber: party.phoneNumber || (
+          party.contact?.phoneNumber
+            ? `${party.contact.areaCode}${party.contact.phoneNumber}`
+            : ''),
         phoneExtension: ''
       }
       setMhrExemptionValue({ key: 'submittingParty', value: partyState })
