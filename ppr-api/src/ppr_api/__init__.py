0bc1212d9c43de11db2638a5221a30c4fc6cd888
thor <1042854+thorwolpert@users.noreply.github.com>
2026-06-17 22:24:27 -0700

    fix: linting

--- ppr-api/src/ppr_api/__init__.py
+++ ppr-api/src/ppr_api/__init__.py
@@ -47,8 +47,9 @@ def create_app(service_environment=APP_RUNNING_ENVIRONMENT, run_mode=None, **kwa
 
     errorhandlers.init_app(app)
 
-    if app.config.get("CLOUDSQL_INSTANCE_CONNECTION_NAME") \
-      and not app.config.get("CLOUDSQL_IAP_SIDECAR"):  # pragma: no cover
+    if app.config.get("CLOUDSQL_INSTANCE_CONNECTION_NAME") and not app.config.get(
+        "CLOUDSQL_IAP_SIDECAR"
+    ):  # pragma: no cover
         from cloud_sql_connector import DBConfig
 
         db_config = DBConfig(
